63028249cec71d4e3b77f6569474ef3f51483191
Basil Starsky <30290462+bs6@users.noreply.github.com>
2019-06-29 20:41:50 +1000

    comment out printing of class

--- hello-world/src/main/kotlin/hw.kt
+++ hello-world/src/main/kotlin/hw.kt
@@ -1,5 +1,6 @@
 package hello.world
 fun main(vararg args: String) {
-	println(args::class)
+	// if you curios, uncomment it to check what type is args of:
+	//println(args::class)
 	println("Hello, world!")
 }
